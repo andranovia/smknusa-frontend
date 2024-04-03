fc5e9f9379db6c218981a7f4496411596043c5eb
iqbal <iqbalagil@gmai.com>
2024-04-03 11:08:24 +0700

    feat: add pagination, fixed postion hero,home section, and footer

---
+++ src/components/berita/news-pagination.tsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+interface PaginationProps {
+  totalPosts: number;
+  postsPerPage: number;
+  onPageChange?: (pageNumber: number) => void; // Optional callback for updating current page
+}
+
+const Pagination: React.FC<PaginationProps> = ({
+  totalPosts,
+  postsPerPage,
+  onPageChange,
+}) => {
+  const [currentPage, setCurrentPage] = useState(1); // State for current page
+
+  const totalPages = Math.ceil(totalPosts / postsPerPage);
+  const pages = [];
+
+  for (let i = 1; i <= totalPages; i++) {
+    pages.push(i);
+  }
+
+  const handleButtonClick = (pageNumber : number) => {
+    if (pageNumber > 0 && pageNumber <= totalPages) {
+      setCurrentPage(pageNumber);
+      if (onPageChange) {
+        onPageChange(pageNumber);
+      }
+    }
+  };
+
+  return (
+    <div className="flex text-center gap-3 p-3">
+            <p className="mt-2">Previous</p>
+        <button className="p-3 bg-gray-200 rounded-md">
+    <Image
+    alt="arrow-right"
+    src={'/assets/icon/arrow-right.svg'}
+    className="-rotate-180"
+    width={15}
+    height={15}
+    />
+        </button>
+      {pages.map((page, index) => (
+        <button
+          key={index}
+          className={`px-3 py-1 rounded-md ${
+            currentPage === page ? "bg-[#FFD980]" : "bg-white"
+          }`}
+          onClick={() => handleButtonClick(page)}
+        >
+          {page}
+        </button>
+      ))}
+      <button className="p-3 bg-gray-200 rounded-md">
+    <Image
+    alt="arrow-right"
+    src={'/assets/icon/arrow-right.svg'}
+    width={15}
+    height={15}
+    />
+        </button>
+      <p className="mt-2">Next</p>
+    </div>
+  );
+};
+
+export default Pagination;
