2e7844e69458488a45b25a6b5b68441e05ccc757
Andranovia <yeromrenandra07@gmail.com>
2024-04-30 21:02:35 +0700

    style: Add and animating search bar in navigation

---
+++ src/components/navigation/navigation-search.tsx
@@ -0,0 +1,42 @@
+import Image from "next/image";
+import React, { useState } from "react";
+import { motion } from 'framer-motion'
+import { defaultTransition } from "../animation/transition";
+
+interface NavigationSearchProps {
+  show: boolean;
+  isActivePage: boolean;
+}
+
+const NavigationSearch = ({ show, isActivePage }: NavigationSearchProps) => {
+
+  const [searchToggle, setSearchToggle] = useState(false);
+
+  
+
+
+
+  return (
+    <div className={`flex justify-center items-center overflow-hidden bg-[#F7F7F7] ${searchToggle  ? 'bg-opacity-100' : 'bg-opacity-0'} transition-all rounded-[10px]  p-2 gap-2 `}>
+      <motion.input transition={defaultTransition}
+      animate={{x:  40 , opacity: searchToggle? 1 : 0, y: searchToggle? 0 : 40}}
+        type="text"
+        placeholder="Ketikkan kata kunci"
+        className={`focus:outline-none  placeholder:font-[500] placeholder:text-sm bg-transparent`}
+      />
+      <motion.div transition={defaultTransition} onClick={() => setSearchToggle(!searchToggle)} animate={{x: searchToggle? -200 : 0, scale: searchToggle? 0.8 : 1}}>
+      <Image
+        src={"/assets/icon/search.svg"}
+        alt="search"
+        className={`${
+          !show && isActivePage  ? searchToggle? `invert` : "invert-0" : 'invert'
+        } transition-all `}
+        width={22}
+        height={22}
+      />
+      </motion.div>
+    </div>
+  );
+};
+
+export default NavigationSearch;
