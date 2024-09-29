f7336d470e3b1356f2cdf5816feaba5a0fc4d5e6
Myre <yeromrenandra07@gmail.com>
2024-09-29 05:08:28 -0700

    feat: Fetching school excellences API and use the data

--- src/components/home/home-slider.tsx
+++ src/components/home/home-slider.tsx
@@ -1,34 +1,18 @@
+"use client";
+
 import Image from "next/image";
 import React from "react";
 import Marquee from "react-fast-marquee";
-
-const sliderData = [
-  {
-    text: "Pengajar yang kompeten",
-  },
-  {
-    text: "Akreditasi A",
-  },
-  {
-    text: "SMK bintang lima",
-  },
-  {
-    text: "Alumni berkualitas",
-  },
-  {
-    text: "Berprestasi",
-  },
-  {
-    text: "Fasilitas lengkap",
-  },
-];
+import { useExcellences } from "@/services/api/useQueries/useExcellences";
 
 const HomeSlider = () => {
+  const { excellences } = useExcellences();
+
   return (
     <section className="text-blue-base w-full flex justify-center overflow-hidden relative  items-center  rounded-md bg-white py-4 ">
       <div className="absolute left-0 bg-gradient-to-r from-white to-transparent z-10 p-10"></div>
       <Marquee autoFill={true} speed={80}>
-        {sliderData.map((data, index) => (
+        {excellences?.map((data, index) => (
           <React.Fragment key={index}>
             <div className="flex items-center ">
               <Image
@@ -38,7 +22,7 @@ const HomeSlider = () => {
                 height={70}
                 className="mx-10 xl:w-16 w-8"
               />
-              <h2 className="font-[500] xl:text-[22px]">{data.text}</h2>
+              <h2 className="font-[500] xl:text-[22px]">{data.title}</h2>
             </div>
           </React.Fragment>
         ))}
